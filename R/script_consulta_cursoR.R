2d3adae84296f968b8c2e96362deb0a32d42a49d
Fabrício Giordanelli <67201324+fgiordanelli@users.noreply.github.com>
2020-12-31 15:04:57 -0300

    update Fabrício

--- R/script_consulta_cursoR.R
+++ R/script_consulta_cursoR.R
@@ -1,6 +1,7 @@
 library(dplyr)
 
-load("df_arrec.rda")
+
+load("C:/Users/Fabrício Giordanelli/Desktop/ALFAPROMOTORA/df_arrec.rda")
 
 # Se quiser simular dados para teste
 
@@ -12,29 +13,36 @@ df_teste <- cbind.data.frame(moeda, valorAuto)
 # função
 
 converter <- function(moeda, valor, x = 1000, y= 2.75, z=1.0641){
-  if(moeda == "UFIR"){
-    new_valor <- valor * z # Valor da ufir em 12/2020
-  } else if (moeda == "OTN"){
-     new_valor <- (valor * 6.17 * 126.8621)/597.06 * z
-  } else if (moeda == "BTN"){
-    new_valor <- valor * 126.8621/597.06 * z
-  } else if (moeda == "MVR"){
-    new_valor <- valor * 63.49 # Valor da MVR em reais
-  } else if(moeda == "Cruzeiro (70 a 86)"){
-    new_valor <- valor / y * x^4
-  } else if (moeda == "Cruzado"){
-    new_valor <- valor / y * x^3
-  } else if (moeda == "Cruzado Novo"){
-    new_valor <- valor / y * x^2
-  } else if (moeda == "Cruzeiro (90 a 93)"){
-    new_valor <- valor / y * x^2
-  } else if(moeda == "Cruzeiro Real"){
-    new_valor <- valor / y * x
-  } else if (moeda == "Real"){
-    new_valor <- valor
-  }
+  mutate(new_valor = ifelse(moeda == "UFIR",
+                            valor * z,
+                            ifelse(moeda == "OTN",
+                                   (valor * 6.17 * 126.8621)/597.06 * z,
+                            ifelse(moeda == "BTN",
+                                   valor * 126.8621/597.06 * z,
+                            iflese(moeda == "MVR",
+                                   valor * 63.49,
+                            iflese(moeda == "Cruzeiro (70 a 86)",
+                                   valor / y * x^4,
+                            iflese(moeda == "Cruzado",
+                                   valor / y * x^3,
+                            iflese(moeda == "Cruzado Novo",
+                                   valor / y * x^2,
+                            iflese(moeda == "Cruzeiro (90 a 93)",
+                                   valor / y * x^2,
+                            iflese(moeda == "Cruzeiro Real",
+                                   valor / y * x,
+                                   valor
+                            )
+                            )
+                            )
+                            )
+                            )
+                            )
+                            )
+                            )
+                            )
+  )
   df <- tibble(valor, moeda, new_valor)
-  return(df)
 }
 
 # atribuindo objeto para conferir resultado
@@ -46,4 +54,3 @@ teste %>% View()
 
 # atribuindo objeto para conferir resultado
 teste <- converter(moeda = df_teste$moeda, valor = df_teste$valorAuto)
-
